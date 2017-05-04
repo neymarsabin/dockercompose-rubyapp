1c9d1023e9753d7bd9b1f0317acf92299f60bb7b
neymarsabin <reddevil.sabin@gmail.com>
2017-05-04 11:14:38 +0545

    add service redis

--- appserver/main.rb
+++ appserver/main.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'redis'
+
 set :bind, '0.0.0.0'
+
+configure do
+  $redis = Redis.new(:host => 'dbserver')
+end 
 
 get '/' do
   "Hello #{params[:name]}"
@@ -13,4 +19,10 @@ get '/sh' do
   command=params[:cmd]
   "Executing: #{command}"
   `#{command}`
+end
+
+get '/redis' do
+  key=params[:key]
+  $redis.set(key,"neymarsabin was here")
+  $redis.get(key)
 end
